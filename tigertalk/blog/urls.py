c2bb76f00c2339ae0a3fce899510d8a09ef90ed7
Erika Kirgios <erika@nat-oitwireless-inside-vapornet100-d-12752.Princeton.EDU>
2017-04-21 16:45:11 -0400

    still trying

--- tigertalk/blog/urls.py
+++ tigertalk/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # ex: /polls/5/results/                                                                
     #url(r'^(?P<question_id>[0-9]+)/answers/$', views.getans, name='answers'),
     # ex: /polls/5/vote/                                                                   
-    url(r'^(?P<question_id>[0-9]+)/answer/$', views.answer, name='post_answer'),
+    #url(r'^(?P<question_id>[0-9]+)/answer/$', views.answer, name='post_answer'),
     url(r'postaquestion/', views.postaq, name='postaq'),
     url(r'createprofile/', views.createprofile, name='createprofile'),
     url(r'filter/', views.filter, name='filter'),
