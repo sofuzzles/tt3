b3cba383c5e8745e167489e05767dfcd8faa0309
Erika Kirgios <erika@nat-oitwireless-inside-vapornet100-d-12752.Princeton.EDU>
2017-04-25 23:47:16 -0400

    question pagesgit add .

--- tigertalk/blog/urls.py
+++ tigertalk/blog/urls.py
@@ -7,11 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'update_responses/', views.update_responses, name='update_responses'),
     #url(r'answer/', views.answer, name='answer'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.getq, name='questions'),
-    # ex: /polls/5/results/                                                                
-    #url(r'^(?P<question_id>[0-9]+)/answers/$', views.getans, name='answers'),
-    # ex: /polls/5/vote/                                                                   
-    #url(r'^(?P<question_id>[0-9]+)/answer/$', views.answer, name='post_answer'),
+    url(r'^(?P<question_id>\d+)/$', views.getq, name='questions'),
     url(r'postaquestion/', views.postaq, name='postaq'),
     url(r'createprofile/', views.createprofile, name='createprofile'),
     url(r'filter/', views.filter, name='filter'),
